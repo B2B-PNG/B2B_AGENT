c6b46932ebb47caed1b6dbd824df56e94524e7e6
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-04-11 11:14:36 +0700

    feat/chinh/generic-filter

--- src/sections/tour/tour-view.tsx
+++ src/sections/tour/tour-view.tsx
@@ -5,41 +5,41 @@ import DestinationAccordion from "./components/destination-accordion";
 import Partner from "./components/partner";
 import { GenericFilter } from "./components/generic-filter/generic-filter";
 import { useState } from "react";
-import TourLocationDes from "./components/tour-location-des";
+// import TourLocationDes from "./components/tour-location-des";
 
 const TourView = () => {
     const [filters, setFilters] = useState<any>({
         keyword: "",
     });
 
-    const [searchResult, setSearchResult] = useState<any[]>([]);
-    const [loading, setLoading] = useState(false);
+    // const [searchResult, setSearchResult] = useState<any[]>([]);
+    // const [loading, setLoading] = useState(false);
 
     const handleSearch = () => {
         console.log("search filters:", filters);
     };
 
-    const handleKeywordChange = async (val: string) => {
-        setFilters((p: any) => ({ ...p, keyword: val }));
+    // const handleKeywordChange = async (val: string) => {
+    //     setFilters((p: any) => ({ ...p, keyword: val }));
 
-        if (!val) {
-            setSearchResult([]);
-            return;
-        }
+    //     if (!val) {
+    //         setSearchResult([]);
+    //         return;
+    //     }
 
-        setLoading(true);
+    //     setLoading(true);
 
-        try {
-            const res = await fetch(`/api/search?keyword=${val}`);
-            const data = await res.json();
+    //     try {
+    //         const res = await fetch(`/api/search?keyword=${val}`);
+    //         const data = await res.json();
 
-            setSearchResult(data);
-        } finally {
-            setLoading(false);
-        }
-    };
+    //         setSearchResult(data);
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // };
 
-    console.log("handleKeywordChange", handleKeywordChange)
+    // console.log("handleKeywordChange", handleKeywordChange)
     return (
         <div className="">
             <BannerSlider slides={slidesTour} />
@@ -49,21 +49,21 @@ const TourView = () => {
             <GenericFilter
                 filters={[
                     { type: "toggle", key: "series", label: "Tour Series" },
-                    {
-                        type: "search",
-                        key: "keyword",
-                        label: "Search",
-                        renderDropdown: ({ value, close }) => (
-                            <TourLocationDes
-                                data={searchResult}
-                                isLoading={loading}
-                                onSelectDestination={(val) => {
-                                    setFilters((p: any) => ({ ...p, keyword: val }));
-                                    close();
-                                }}
-                            />
-                        ),
-                    },
+                    // {
+                    //     type: "search",
+                    //     key: "keyword",
+                    //     label: "Search",
+                    //     renderDropdown: ({ value, close }) => (
+                    //         <TourLocationDes
+                    //             data={searchResult}
+                    //             isLoading={loading}
+                    //             onSelectDestination={(val) => {
+                    //                 setFilters((p: any) => ({ ...p, keyword: val }));
+                    //                 close();
+                    //             }}
+                    //         />
+                    //     ),
+                    // },
 
                     { type: "guest", key: "guest" },
                     { type: "dateRange", keyStart: "start", keyEnd: "end" },
