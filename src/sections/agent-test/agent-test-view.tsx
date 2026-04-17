7b7156952f9eaa93f54006c98da1f3ac52d99e5a
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-04-17 16:30:16 +0700

    fix

--- src/sections/agent-test/agent-test-view.tsx
+++ src/sections/agent-test/agent-test-view.tsx
@@ -5,7 +5,7 @@ import { paths } from "@/routes/paths";
 
 const AgentTestView = () => {
     const router = useRouter()
-    const { user, userLoading } = useUser()
+    const { user } = useUser()
     const data = [
         { id: 11, name: 'CÔNG TY KẾT NỐI DU LỊCH', strCompanyNameUrl: "cong-ty-tnhh-ket-noi-du-lich-8F620" },
     ];
