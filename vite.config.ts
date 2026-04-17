2f4ab2a39b1c328b89d8362ad66843a0d3a259c9
chinhhoangtc <chinhhoangtc03@gmail.com>
2026-04-17 13:56:13 +0700

    fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  base: './',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
